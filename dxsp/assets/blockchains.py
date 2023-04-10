3e00fca74d53c27f35888596c0eaf6e8dace70a0
mraniki <mraniki@users.noreply.github.com>
2023-04-10 22:40:06 +0200

    update for focus on 7 chains mainnet and testnet

--- dxsp/assets/blockchains.py
+++ dxsp/assets/blockchains.py
@@ -3,160 +3,129 @@ blockchains = {
     1: {
         "url": "https://api.etherscan.io/api?",
         "rpc": "https://rpc.ankr.com/eth",
-    },
-    # ETHEREUM Ropsten
-    3: {
-        "url": "https://api-Ropsten.etherscan.io/api?",
-        "rpc": "https://rpc.ankr.com/eth",
-    },
-    # ETHEREUM Rinkeby
-    4: {
-        "url": "https://api-Rinkeby.etherscan.io/api?",
-        "rpc": "https://rpc.ankr.com/eth",
+        "uniswap_v2": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+        "uniswap_v3": "0x1f98431c8ad98523631ae4a59f267346ea31f984",
+        "1inch": "Y",
+        "0x": "https://api.0x.org/"
     },
     # ETHEREUM Gorli
     5: {
         "url": "https://api-Goerli.etherscan.io/api?",
-        "rpc": "https://rpc.ankr.com/eth",
-    },
-    # Ethereum Classic Kotti
-    6: {
-        "url": "https://blockscout.com/etc/kotti/api?",
-        "rpc": "https://rpc.ankr.com/eth",
-    },
-    # Optimism
-    10: {
-        "url": "https://api-optimistic.etherscan.io/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
-    },
-    # CRONOS
-    25: {
-        "url": "https://api.cronoscan.com/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
-    },
-    # RSK
-    30: {
-        "url": "https://blockscout.com/rsk/mainnet/api?",
-        "rpc": "https://rpc.ankr.com/bsc"
-    },
-    # ETHEREUM Kovan
-    42: {
-        "url": "https://api-Kovan.etherscan.io/api?",
-        "rpc": "https://rpc.ankr.com/bsc"
-    },
-    # Binance Smart Chain
-    56: {
-        "url": "https://api.bscscan.com/api?",
-        "rpc": "https://rpc.ankr.com/bsc"
-    },
-    # Optimism kovan
-    69: {
-        "url": "https://api-kovan-optimistic.etherscan.io/api?",
-        "rpc": "https://rpc.ankr.com/bsc"
-    },
-    # Hoo Smart Chain
-    70: {
-        "url": "https://hooscan.com/api?",
-        "rpc": "https://rpc.ankr.com/bsc"
-    },
-    # Binance Smart Chain Testnet
-    97: {
-        "url": "https://api-testnet.bscscan.com/api?",
-        "rpc": "https://rpc.ankr.com/bsc"
-    },
-    # Gnosis
-    100: {
-        "url": "https://blockscout.com/xdai/mainnet/api?",
-        "rpc": "https://rpc.ankr.com/gnosis"
-    },
-    # Polygon
-    137: {
-        "url": "https://api.polygonscan.com/api?",
-        "rpc": "https://rpc.ankr.com/polygon",
-    },
-    # Fantom
-    250: {
-        "url": "https://api.ftmscan.com/api?",
-        "rpc": "https://rpc.ankr.com/fantom",
-    },
-    # Cronos Testnet
-    338: {
-        "url": "https://api-testnet.cronoscan.com/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
-    },
-    #  Moonbeam
-    1284: {
-        "url": "https://api-moonbeam.moonscan.io/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
-    },
-    #  Moonriver
-    1285: {
-        "url": "https://api-moonriver.moonscan.io/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
-    },
-    #  Moonbase
-    1287: {
-        "url": "https://api-moonbase.moonscan.io/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
-    },
-    # Fantom Testnet
-    4002: {
-        "url": "https://api-testnet.ftmscan.com/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
-    },
-    # Arbitrum One
-    42161: {
-        "url": "https://api.arbiscan.io/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
-    },
-    # Celo
-    42220: {
-        "url": "https://api.celoscan.xyz/api?",
-        "rpc": "https://rpc.ankr.com/celo",
-    },
-    #  Avalanche Fuji Testnet
-    43113: {
-        "url": "https://api-testnet.snowtrace.io/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
-    },
-    # Avalanche
-    43114: {
-        "url": "https://api.snowtrace.io/api?",
-        "rpc": "https://rpc.ankr.com/avalanche",
-    },
-    # Celo Alfajores
-    44787: {
-        "url": "https://api-alfajores.celoscan.xyz/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
-    },
-    # Polygon Mumbai
-    80001: {
-        "url": "https://api-testnet.polygonscan.com/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
-    },
-    # Artis sigma1
-    246529: {
-        "url": "https://blockscout.com/artis/sigma1/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
-    },
-    # Arbitrum Rinkeby
-    421611: {
-        "url": "https://api-testnet.arbiscan.io/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
+        "rpc": "https://rpc.ankr.com/eth_goerli",
+        "uniswap_v2": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+        "uniswap_v3": "",
+        "1inch": "",
+        "0x": "https://goerli.api.0x.org/"
     },
     # ETHEREUM Sepolia
     11155111: {
         "url": "https://api-Sepolia.etherscan.io/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
+        "rpc": "https://rpc.ankr.com/eth_sepolia",
+        "uniswap_v2": "",
+        "uniswap_v3": "0x1f98431c8ad98523631ae4a59f267346ea31f984",
+        "1inch": ""
+        },
+    # Binance Smart Chain
+    56: {
+        "url": "https://api.bscscan.com/api?",
+        "rpc": "https://rpc.ankr.com/bsc",
+        "uniswap_v2": "0xca143ce32fe78f1f7019d7d551a6402fc5350c73",
+        "uniswap_v3": "",
+        "1inch": "Y",
+        "0x": "https://bsc.api.0x.org/"
+        },
+    # Binance Smart Chain Testnet
+    97: {
+        "url": "https://api-testnet.bscscan.com/api?",
+        "rpc": "https://rpc.ankr.com/bsc_testnet_chapel",
+        "uniswap_v2": "",
+        "uniswap_v3": "",
+        "1inch": ""
+        },
+    # Arbitrum One
+    42161: {
+        "url": "https://api.arbiscan.io/api?",
+        "rpc": "https://rpc.ankr.com/arbitrum",
+        "uniswap_v2": "0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f",
+        "uniswap_v3": "0x1f98431c8ad98523631ae4a59f267346ea31f984",
+        "1inch": "Y",
+        "0x": "https://arbitrum.api.0x.org/"
+        },
+    # Arbitrum Rinkeby
+    421611: {
+        "url": "https://api-testnet.arbiscan.io/api?",
+        "rpc": "https://rinkeby.arbitrum.io/rpc",
+        "uniswap_v2": "",
+        "uniswap_v3": "",
+        "1inch": ""
+        },
+    # Polygon
+    137: {
+        "url": "https://api.polygonscan.com/api?",
+        "rpc": "https://rpc.ankr.com/polygon",
+        "uniswap_v2": "0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f",
+        "uniswap_v3": "0x1f98431c8ad98523631ae4a59f267346ea31f984",
+        "1inch": "Y",
+        "0x": "https://polygon.api.0x.org/"
     },
-    # Aurora
-    1313161554: {
-        "url": "https://api.aurorascan.dev/api?",
+    # Polygon Mumbai
+    80001: {
+        "url": "https://api-testnet.polygonscan.com/api?",
+        "rpc": "https://rpc.ankr.com/polygon_mumbai",
+        "uniswap_v2": "",
+        "uniswap_v3": "0x1f98431c8ad98523631ae4a59f267346ea31f984",
+        "1inch": "",
+        "0x": "https://mumbai.api.0x.org/"
+        },
+    # Optimism
+    10: {
+        "url": "https://api-optimistic.etherscan.io/api?",
         "rpc": "https://rpc.ankr.com/optimism",
+        "uniswap_v2": "0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f",
+        "uniswap_v3": "0x1f98431c8ad98523631ae4a59f267346ea31f984",
+        "1inch": "Y",
+        "0x": "https://optimism.api.0x.org/"
     },
-    # Aurora Testnet
-    1313161555: {
-        "url": "https://api-testnet.aurorascan.dev/api?",
-        "rpc": "https://rpc.ankr.com/optimism",
-    },
+    # Optimism goerli
+    69: {
+        "url": "https://api-Goerli.etherscan.io/api? ne",
+        "rpc": "https://rpc.ankr.com/optimism_testnet",
+        "uniswap_v2": "",
+        "uniswap_v3": "0x1f98431c8ad98523631ae4a59f267346ea31f984",
+        "1inch": ""
+        },
+    # Avalanche
+    43114: {
+        "url": "https://api.snowtrace.io/api?",
+        "rpc": "https://rpc.ankr.com/avalanche",
+        "uniswap_v2": "0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f",
+        "uniswap_v3": "0x1f98431c8ad98523631ae4a59f267346ea31f984",
+        "1inch": "Y",
+        "0x": "https://avalanche.api.0x.org/"
+        },
+    #  Avalanche Fuji Testnet
+    43113: {
+        "url": "https://api-testnet.snowtrace.io/api?",
+        "rpc": "https://rpc.ankr.com/avalanche_fuji",
+        "uniswap_v2": "",
+        "uniswap_v3": "0x1f98431c8ad98523631ae4a59f267346ea31f984",
+        "1inch": ""
+        },
+    # Fantom
+    250: {
+        "url": "https://api.ftmscan.com/api?",
+        "rpc": "https://rpc.ankr.com/fantom",
+        "uniswap_v2": "0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f",
+        "uniswap_v3": "0x1f98431c8ad98523631ae4a59f267346ea31f984",
+        "1inch": "Y",
+        "0x": "https://fantom.api.0x.org/"
+        },
+    # Fantom Testnet
+    4002: {
+        "url": "https://api-testnet.ftmscan.com/api?",
+        "rpc": "https://rpc.ankr.com/fantom_testnet",
+        "uniswap_v2": "",
+        "uniswap_v3": "0x1f98431c8ad98523631ae4a59f267346ea31f984",
+        "1inch": ""
+        },
 }
