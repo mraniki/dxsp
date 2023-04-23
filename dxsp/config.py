32962207968a37cef54843953d2404b520962995
mraniki <8766259+mraniki@users.noreply.github.com>
2023-04-23 08:48:45 +0200

    Update config.py

--- dxsp/config.py
+++ dxsp/config.py
@@ -4,4 +4,7 @@ from dynaconf import Dynaconf
 settings = Dynaconf(
     envvar_prefix="DXSP",
     settings_files=['core.toml','settings.toml', '.secrets.toml'],
+    load_dotenv=True,
+    environments=True,
+    default_env="default",
 )
