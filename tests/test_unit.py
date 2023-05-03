9d82587236724d041486a25b1cf0277587e5e1b0
mraniki <8766259+mraniki@users.noreply.github.com>
2023-05-03 16:41:27 +0200

    fix: unit test setup

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,23 +1,8 @@
 import pytest
-from unittest.mock import patch, MagicMock
+from unittest.mock import Mock, patch, MagicMock
 from dxsp import DexSwap
 
 @pytest.fixture
-def my_dex():
-    return DexSwap()
+def dex_swap():
+    return DexSwap(chain_id=1)
 
-@pytest.mark.asyncio
-async def test__get(my_class):
-    url = "https://example.com"
-    params = {"param1": "value1", "param2": "value2"}
-    headers = {"header1": "value1", "header2": "value2"}
-
-    with patch("dxsp.requests.get") as mock_get:
-        mock_response = MagicMock()
-        mock_response.json.return_value = {"key": "value"}
-        mock_get.return_value = mock_response
-
-        result = await DexSwap._get(url, params=params, headers=headers)
-
-        mock_get.assert_called_once_with(url, params=params, headers=headers, timeout=10)
-        assert result == {"key": "value"}
