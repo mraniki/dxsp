432fa17fc3587a6a5eb2939c2d29e6c7bb9183aa
mraniki <mraniki@users.noreply.github.com>
2023-05-07 23:03:20 +0200

    🥅 Add tests for DexSwap class methods

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest.mock import MagicMock
 from dxsp import DexSwap
 
 
@@ -75,3 +76,70 @@ async def test_logger(caplog):
         assert record.levelname != "CRITICAL"
         assert "wally" not in caplog.text
 
+
+@pytest.mark.asyncio
+async def test_uniswap_v2_quote():
+    # Mock the router instance
+    router_instance = MagicMock()
+    router_instance.functions.getAmountsOut.return_value.call.return_value = [0, 1234]
+
+    exchange = DexSwap()
+
+    # Call the function being tested
+    result = await exchange.uniswap_v2_quote("asset_in_address", "asset_out_address", amount=1)
+
+    # Check the result
+    assert result == 1234
+
+
+@pytest.mark.asyncio
+async def test_oneinch_swap():
+    # Mock the _get method
+    exchange = DexSwap()
+    exchange._get = MagicMock(return_value={"statusCode": 200})
+
+    result = await exchange.oneinch_swap("asset_out_address", "asset_in_address", "100")
+
+    assert result == {"statusCode": 200}
+
+
+@pytest.mark.asyncio
+async def test_get_confirmation():
+    # Mock the required attributes and methods
+    exchange = DexSwap()
+    exchange.logger = MagicMock()
+    exchange.w3.eth.get_transaction_receipt = MagicMock(return_value={"gasUsed": 100})
+    exchange.w3.eth.get_block = MagicMock(return_value={"timestamp": 12345})
+    order_hash_details = {"blockNumber": 123}
+    asset_out_symbol = "ETH"
+    asset_out_address = "0x123"
+    order_amount = 1
+
+    # Call the function being tested
+    result = await exchange.get_confirmation(
+    "order_hash",
+    order_hash_details,
+    asset_out_symbol,
+    asset_out_address,
+    order_amount)
+
+    # Check the result
+    assert "id" in result
+    assert "timestamp" in result
+    assert "instrument" in result
+    assert "contract" in result
+    assert "amount" in result
+    assert "fee" in result
+    assert "price" in result
+    assert "confirmation" in result
+
+
+@pytest.mark.asyncio
+async def test_execute_order():
+    exchange = DexSwap()
+    # call the function and check the output
+    order_params = {"action": "BUY", "instrument": "ETH", "quantity": 1}
+    result = await exchange.execute_order(order_params)
+    assert isinstance(result, str)
+    
+    
