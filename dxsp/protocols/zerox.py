609f410070dba7b98febacb6d9e27171e1a5167e
mraniki <mraniki@users.noreply.github.com>
2023-06-29 08:48:40 +0200

    :bug:  protocol

--- dxsp/protocols/zerox.py
+++ dxsp/protocols/zerox.py
@@ -11,7 +11,7 @@ async def get_zerox_quote(cls, buy_address, sell_address, amount=1):
         out_amount = cls.w3.to_wei(amount, 'ether')
         url = f"{settings.dex_0x_url}/swap/v1/quote?buyToken={str(buy_address)}&sellToken={str(sell_address)}&buyAmount={str(out_amount)}"
         headers = {"0x-api-key": settings.dex_0x_api_key}
-        response = await self.get(url, params=None, headers=headers)
+        response = await cls.get(url, params=None, headers=headers)
         print(response)
         if response:
             return round(float(response['guaranteedPrice']), 3)
