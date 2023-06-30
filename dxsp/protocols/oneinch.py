ccb7373aa3af3ece09762583147581a2ec382614
mraniki <8766259+mraniki@users.noreply.github.com>
2023-06-30 17:05:49 +0200

    💬 commenting for 1inch

--- dxsp/protocols/oneinch.py
+++ dxsp/protocols/oneinch.py
@@ -37,8 +37,8 @@ class DexSwapOneInch(DexSwap):
         #         quote = self.w3.from_wei(int(quote_amount), 'ether')
         #         # /(10 ** quote_decimals))
         #         return round(quote, 2)
-        # except Exception as error:
-        #     raise ValueError(f"Approval failed {error}") 
+        except Exception as error:
+            raise ValueError(f"Approval failed {error}") 
 
     async def get_approve(self, token_address):
         # pass
