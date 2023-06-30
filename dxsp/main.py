bb9693cd5f7f7cd43a0808a9f8bae729823ed585
mraniki <8766259+mraniki@users.noreply.github.com>
2023-06-30 17:31:12 +0200

    🎨 code format

--- dxsp/main.py
+++ dxsp/main.py
@@ -203,32 +203,6 @@ class DexSwap:
             raise error
 
 
-    async def get_abi(self, address):
-        if not settings.dex_block_explorer_api:
-            self.logger.warning("No block_explorer_api.")
-            return None
-
-        params = {
-            "module": "contract",
-            "action": "getabi",
-            "address": address,
-            "apikey": settings.dex_block_explorer_api
-        }
-
-        try:
-            resp = await self.get(
-                url=settings.dex_block_explorer_url, params=params)
-            if resp['status'] == "1":
-                self.logger.debug("ABI found %s", resp)
-                return resp["result"]
-            else:
-                self.logger.warning("No ABI identified")
-                return None
-        except Exception as error:
-            self.logger.error("get_abi %s", error)
-            return None
-
-
     async def calculate_sell_amount(self, sell_token_address, quantity):
         """Calculates the sell amount based on the sell token balance and trading risk amount."""
         sell_balance = await self.get_token_balance(sell_token_address)
@@ -249,7 +223,7 @@ class DexSwap:
                 "timestamp": block["timestamp"],
                 "id": receipt["transactionHash"],
                 "instrument": receipt["to"],
-                "contract": receipt["to"],
+                "contract": receipt["to"], # TBD To be determined.
                 "amount": transaction["value"],
                 "price": transaction["value"],  # TBD To be determined.
                 "fee": receipt["gasUsed"],
@@ -263,26 +237,6 @@ class DexSwap:
             }
         except Exception as error:
             raise error
-
-    'difficulty': 49824742724615,
-    'extraData': '0xe4b883e5bda9e7a59ee4bb99e9b1bc',
-    'gasLimit': 4712388,
-    'gasUsed': 21000,
-    'hash': '0xc0f4906fea23cf6f3cce98cb44e8e1449e455b28d684dfa9ff65426495584de6',
-    'logsBloom': '0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
-    'miner': '0x61c808d82a3ac53231750dadc13c777b59310bd9',
-    'nonce': '0x3b05c6d5524209f1',
-    'number': 2000000,
-    'parentHash': '0x57ebf07eb9ed1137d41447020a25e51d30a0c272b5896571499c82c33ecb7288',
-    'receiptRoot': '0x84aea4a7aad5c5899bd5cfc7f309cc379009d30179316a2a7baa4a2ea4a438ac',
-    'sha3Uncles': '0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347',
-    'size': 650,
-    'stateRoot': '0x96dbad955b166f5119793815c36f11ffa909859bbfeb64b735cca37cbf10bef1',
-    'timestamp': 1470173578,
-    'totalDifficulty': 44010101827705409388,
-    'transactions': ['0xc55e2b90168af6972193c1f86fa4d7d7b31a29c156665d15b9cd48618b5177ef'],
-    'transactionsRoot': '0xb31f174d27b99cdae8e746bd138a01ce60d8dd7b224f7c60845914def05ecc58',
-    'uncles': [],
 
     async def get_gas(self, transaction):
         """get gas estimate"""
@@ -376,6 +330,32 @@ class DexSwap:
         return self.w3.eth.contract(
             address=token_address,
             abi=token_abi)
+
+    async def get_abi(self, address):
+        if not settings.dex_block_explorer_api:
+            self.logger.warning("No block_explorer_api.")
+            return None
+
+        params = {
+            "module": "contract",
+            "action": "getabi",
+            "address": address,
+            "apikey": settings.dex_block_explorer_api
+        }
+
+        try:
+            resp = await self.get(
+                url=settings.dex_block_explorer_url, params=params)
+            if resp['status'] == "1":
+                self.logger.debug("ABI found %s", resp)
+                return resp["result"]
+            else:
+                self.logger.warning("No ABI identified")
+                return None
+        except Exception as error:
+            self.logger.error("get_abi %s", error)
+            return None
+
 
 # 🔒 USER RELATED
     async def get_token_balance(self, token_address: str) -> Optional[int]:
