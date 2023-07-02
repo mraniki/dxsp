7fc6543275f120e185109d25f592ddb2b39ad01a
mraniki <mraniki@users.noreply.github.com>
2023-07-02 11:18:13 +0200

    🥚 using uniswap-python for the uni class

--- dxsp/protocols/uniswap.py
+++ dxsp/protocols/uniswap.py
@@ -1,30 +1,35 @@
 """
-uniswap V2  🦄
+uniswap  🦄
 """
 from dxsp.config import settings
 from dxsp.main import DexSwap
+from uniswap import Uniswap
+
+
 
 class DexSwapUniswap(DexSwap):
     async def get_quote(
         self,
         buy_address,
         sell_address,
-        amount=1
+        amount=10
     ):
         try:
-            await self.router_contract()
-            if self.protocol_type == "uniswap_v2":
-                await self.router_contract()
-                path = [sell_address,buy_address]
-                print(path)
-                amount_out = self.router.functions.getAmountsOut(
-                    amount * await self.get_token_decimals(buy_address),
-                    path).call()[-1]
-                quote = int(amount_out / await self.get_token_decimals(sell_address))
-                return quote
+            uniswap = Uniswap(address=self.wallet_address, private_key=self.private_key, version=2, web3=self.w3, router_contract_addr=settings.dex_router_contract_addr)
+            uniswap.get_price_input(sell_address, buy_address, amount ** await self.get_token_decimals(buy_address))
+            # await self.router_contract()
+            # if self.protocol_type == "uniswap_v2":
+            #     await self.router_contract()
+            #     path = [sell_address,buy_address]
+            #     print(path)
+            #     amount_out = self.router.functions.getAmountsOut(
+            #         amount * await self.get_token_decimals(buy_address),
+            #         path).call()[-1]
+            #     quote = int(amount_out / await self.get_token_decimals(sell_address))
+            #     return quote
 
-            if self.protocol_type == "uniswap_v3":
-                pass
+            # if self.protocol_type == "uniswap_v3":
+            #     pass
                 # await self.quoter_contract()
                 # sqrtPriceLimitX96 = 0
                 # fee = 3000
