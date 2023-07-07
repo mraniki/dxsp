b885407c7e59dfb62506cb71d908a1c12b2c8cf0
mraniki <mraniki@users.noreply.github.com>
2023-07-07 22:26:58 +0200

    :rotating_light: linter

--- dxsp/main.py
+++ dxsp/main.py
@@ -362,7 +362,7 @@ class DexSwap:
 
     async def get_account_pnl(self, frequency="daily"):
 
-        latest_tx_date = await dex.get_block_timestamp(self.w3.eth.get_block('latest'))
+        latest_tx_date = await self.get_block_timestamp(self.w3.eth.get_block('latest'))
 
         #total_profit_loss = 0
 
