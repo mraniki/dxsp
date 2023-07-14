2657bd6cc2651e7de1e9cb42d03859bc0f87844d
mraniki <8766259+mraniki@users.noreply.github.com>
2023-07-14 21:32:21 +0200

    🚨 linter

--- dxsp/protocols/zerox.py
+++ dxsp/protocols/zerox.py
@@ -9,8 +9,10 @@ class DexSwapZeroX(DexSwap):
 
     async def get_quote(self, buy_address, sell_address, amount=1):
         try:
-            out_amount = amount * (10 ** await self.contract_utils.get_token_decimals(sell_address))
-            url = f"{settings.dex_0x_url}/swap/v1/quote?buyToken={str(buy_address)}&sellToken={str(sell_address)}&sellAmount={str(out_amount)}"
+            token_decimals = await self.contract_utils.get_token_decimals(sell_address)
+            out_amount = amount * (10 ** token_decimals)
+            url = (f"{settings.dex_0x_url}/swap/v1/quote"
+                f"?buyToken={str(buy_address)}&sellToken={str(sell_address)}&sellAmount={str(out_amount)}")
             headers = {"0x-api-key": settings.dex_0x_api_key}
             response = await get(url, params=None, headers=headers)
             if response:
