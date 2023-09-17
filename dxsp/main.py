d2293e3dfd05ebcb901738520ecf8fdd7c4ee6db
mraniki <8766259+mraniki@users.noreply.github.com>
2023-09-17 16:35:33 +0200

    ♻️

--- dxsp/main.py
+++ dxsp/main.py
@@ -42,25 +42,31 @@ class DexTrader:
                 logger.debug(f"Loading {cx}")
                 w3 = w3 or Web3(Web3.HTTPProvider(exchanges[cx]["rpc"]))
                 protocol_type = exchanges[cx]["protocol_type"]
-                protocol_version= exchanges[cx]["protocol_version"]
-                api_endpoint= exchanges[cx]["api_endpoint"]
-                api_key= exchanges[cx]["api_key"]
-                router= exchanges[cx]["router_contract_addr"]
-                trading_asset_address= exchanges[cx]["trading_asset_address"]
-                block_explorer_url= exchanges[cx]["block_explorer_url"]
-                block_explorer_api= exchanges[cx]["block_explorer_api"]
-                account_utils= AccountUtils(w3)
-                contract_utils= ContractUtils(w3)
-                gas_strategy= w3.eth.set_gas_price_strategy(medium_gas_price_strategy)
-                client = DexClient(w3=w3,
-                                   protocol_type=protocol_type,
-                                   protocol_version=protocol_version,
-                                   api_endpoint=api_endpoint,
-                                   api_key=api_key,
-                                   router=router,
-                                   trading_asset_address=trading_asset_address,
-                                   block_explorer_url=block_explorer_url,
-                                   block_explorer_api=block_explorer_api)
+                protocol_version = exchanges[cx]["protocol_version"]
+                api_endpoint = exchanges[cx]["api_endpoint"]
+                api_key = exchanges[cx]["api_key"]
+                router = exchanges[cx]["router_contract_addr"]
+                trading_asset_address = exchanges[cx]["trading_asset_address"]
+                block_explorer_url = exchanges[cx]["block_explorer_url"]
+                block_explorer_api = exchanges[cx]["block_explorer_api"]
+                trading_risk_amount = exchanges[cx]["trading_risk_amount"]
+                trading_slippage = exchanges[cx]["trading_slippage"]
+                account_utils = AccountUtils(w3)
+                contract_utils = ContractUtils(w3)
+                gas_strategy = w3.eth.set_gas_price_strategy(medium_gas_price_strategy)
+                client = DexClient(
+                    w3=w3,
+                    protocol_type=protocol_type,
+                    protocol_version=protocol_version,
+                    api_endpoint=api_endpoint,
+                    api_key=api_key,
+                    router=router,
+                    trading_asset_address=trading_asset_address,
+                    trading_risk_amount = trading_risk_amount,
+                    trading_slippage = trading_slippage,
+                    block_explorer_url=block_explorer_url,
+                    block_explorer_api=block_explorer_api,
+                )
                 self.dex_info.append(
                     {
                         "w3": w3,
@@ -71,11 +77,13 @@ class DexTrader:
                         "router": router,
                         "client": client,
                         "trading_asset_address": trading_asset_address,
+                        "trading_risk_amount": trading_risk_amount,
+                        "trading_slippage": trading_slippage,
                         "block_explorer_url": block_explorer_url,
                         "block_explorer_api": block_explorer_api,
                         "account_utils": account_utils,
                         "contract_utils": contract_utils,
-                        "gas_strategy": gas_strategy
+                        "gas_strategy": gas_strategy,
                     }
                 )
 
@@ -94,17 +102,17 @@ class DexTrader:
 
         """
         try:
-            for exchange in self.dex_info:
+            for dx in self.dex_info:
                 logger.debug("execute order")
                 action = order_params.get("action")
                 instrument = order_params.get("instrument")
                 quantity = order_params.get("quantity", 1)
                 sell_token, buy_token = (
-                    (self.account.trading_asset_address, instrument)
+                    (dx["trading_asset_address"], instrument)
                     if action == "BUY"
-                    else (instrument, self.account.trading_asset_address)
+                    else (instrument, dx["trading_asset_address"])
                 )
-                order = await self.get_swap(sell_token, buy_token, quantity)
+                order = await self.get_swap(dx["client"],sell_token, buy_token, quantity)
                 if order:
                     trade_confirmation = (
                         f"⬇️ {instrument}"
@@ -117,8 +125,7 @@ class DexTrader:
         except Exception as error:
             return f"⚠️ order execution: {error}"
 
-
-    async def get_swap(self, sell_token: str, buy_token: str, quantity: int) -> None:
+    async def get_swap(self, dex_client, sell_token: str, buy_token: str, quantity: int) -> None: 
         """
         Execute a swap
 
@@ -138,32 +145,32 @@ class DexTrader:
             logger.debug("sell token {}", sell_token_address)
             logger.debug("sell token {}", sell_token_address)
             if not sell_token.startswith("0x"):
-                sell_token_address = await self.contract_utils.search_contract_address(
+                sell_token_address = await dex_client.search_contract_address(
                     sell_token
                 )
             buy_token_address = buy_token
             logger.debug("buy token {}", buy_token_address)
             logger.debug("buy token {}", buy_token_address)
             if not buy_token_address.startswith("0x"):
-                buy_token_address = await self.contract_utils.search_contract_address(
+                buy_token_address = await dex_client.["contract_utils"].search_contract_address(
                     buy_token
                 )
-            sell_amount = await self.contract_utils.calculate_sell_amount(
-                sell_token_address, self.account.wallet_address, quantity
+            sell_amount = await dex_client.["contract_utils"].calculate_sell_amount(
+                sell_token_address, dex_client.["account_utils"].wallet_address, quantity
             )
             sell_token_amount_wei = sell_amount * (
-                10 ** (await self.contract_utils.get_token_decimals(sell_token_address))
+                10 ** (await dex_client.["account_utils"].get_token_decimals(sell_token_address))
             )
             if self.protocol_type == "0x":
-                await self.account.get_approve(sell_token_address)
+                await dex_client.["account_utils"].get_approve(sell_token_address)
 
             order_amount = int(
                 sell_token_amount_wei
-                * decimal.Decimal((settings.dex_trading_slippage / 100))
+                * decimal.Decimal((dx["trading_slippage"] / 100))
             )
             logger.debug(order_amount)
             logger.debug(order_amount)
-            order = await self.dex_swap.get_swap(
+            order = await dex_client.get_swap(
                 sell_token_address, buy_token_address, order_amount
             )
 
@@ -172,16 +179,16 @@ class DexTrader:
                 logger.debug("swap order error")
                 raise ValueError("swap order not executed")
 
-            signed_order = await self.account.get_sign(order)
-            order_hash = str(self.w3.to_hex(signed_order))
-            receipt = self.w3.wait_for_transaction_receipt(order_hash)
+            signed_order = await dex_client.account.get_sign(order)
+            order_hash = str(dex_client.w3.to_hex(signed_order))
+            receipt = dex_client.w3.wait_for_transaction_receipt(order_hash)
 
             if receipt["status"] != 1:
                 logger.debug(receipt)
                 logger.debug(receipt)
                 raise ValueError("receipt failed")
 
-            return await self.contract_utils.get_confirmation(
+            return await dex_client.["account_utils"].get_confirmation(
                 receipt["transactionHash"]
             )
 
@@ -335,4 +342,3 @@ class DexTrader:
             for the account within the specified period.
         """
         return await self.account.get_account_pnl(period)
-
