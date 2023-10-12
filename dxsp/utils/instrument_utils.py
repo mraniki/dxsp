0593df9773a81223f6712585efbb7669dd30a8a6
mraniki <8766259+mraniki@users.noreply.github.com>
2023-10-12 12:59:05 +0200

    🔥

--- dxsp/utils/instrument_utils.py
+++
@@ -1,9 +0,0 @@
-class Instrument:
-    def __init__(self, name, symbol, address, alt_symbol=None):
-        self.name = name
-        self.symbol = symbol
-        self.address = address
-        self.alt_symbol = alt_symbol
-
-    def __str__(self):
-        return f"{self.name} ({self.symbol})"
