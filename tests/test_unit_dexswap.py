6b55baf433ed2189b36b2de3f2b688278654ce55
mraniki <mraniki@users.noreply.github.com>
2023-10-13 18:38:25 +0200

    Refactor unit tests and fixtures in test_unit.py

--- tests/test_unit_dexswap.py
+++ tests/test_unit_dexswap.py
@@ -22,47 +22,6 @@ def DexSwap_fixture():
     return DexSwap()
 
 
-@pytest.fixture(name="dex_client")
-def mock_dex_client():
-    return DexUniswap(
-        name="uniswap",
-        wallet_address="0x1a9C8182C09F50C8318d769245beA52c32BE35BC",
-        private_key="0xdeadbeet45ab87712ad64ccb3b10217737f7faacbf2872e88fdd9a537d8fe266",
-        protocol="uniswap",
-        protocol_version=2,
-        api_endpoint=None,
-        api_key=None,
-        router_contract_addr="0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-        factory_contract_addr="0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
-        trading_asset_address="0xdAC17F958D2ee523a2206206994597C13D831ec7",
-        trading_slippage=2,
-        trading_risk_amount=10,
-        trading_risk_percentage=True,
-        trading_asset_separator="",
-        block_explorer_url="https://api.etherscan.io/api?",
-        block_explorer_api=None,
-        w3=Web3(Web3.HTTPProvider("https://eth.llamarpc.com")),
-        mapping=None,
-    )
-
-
-@pytest.fixture
-def tester_provider():
-    return EthereumTesterProvider()
-
-
-@pytest.fixture(name="web3")
-def w3():
-    provider = EthereumTesterProvider()
-    return Web3(provider)
-
-
-@pytest.fixture(name="account")
-def account_fixture(web3) -> str:
-    """setup account."""
-    return web3.eth.accounts[0]
-
-
 @pytest.fixture(name="order")
 def order_params_fixture():
     """Return order parameters."""
@@ -145,10 +104,14 @@ async def test_get_quotes(dex):
     assert get_quote.awaited
     assert ("eth" in result) or ("bsc" in result) or ("pol" in result)
     assert "2" in result
-    # assert "proxy" in result
     numerical_count = sum(1 for char in result if char.isdigit())
     assert numerical_count >= 10
 
+@pytest.mark.asyncio
+async def test_get_quotes_invalid(dex):
+    """getquote Testing"""
+    result = await dex.get_quotes("NOTATOKEN")
+    assert "Quote failed" in result    
 
 @pytest.mark.asyncio
 async def test_get_balances(dex):
@@ -187,6 +150,11 @@ async def test_submit_order(dex, order):
     result = await dex.submit_order(order)
     assert result is not None
 
+@pytest.mark.asyncio
+async def test_submit_invalid_symbol(dex, invalid_symbol):
+    result = await dex.submit_order(invalid_symbol)
+    assert result is not None
+
 
 @pytest.mark.asyncio
 async def test_submit_order_invalid(dex, invalid_order):
@@ -194,50 +162,3 @@ async def test_submit_order_invalid(dex, invalid_order):
     assert "⚠️" in result
 
 
-@pytest.mark.asyncio
-async def test_get_swap(dex_client):
-    result = await dex_client.get_swap(sell_token="USDT", buy_token="UNI", quantity=1)
-    assert result is not None
-
-
-@pytest.mark.asyncio
-async def test_get_confirmation(dex_client):
-    result = await dex_client.contract_utils.get_confirmation(
-        "0xda56e5f1a26241a03d3f96740989e432ca41ae35b5a1b44bcb37aa2cf7772771"
-    )
-    print(result)
-    assert result is not None
-    assert result["timestamp"] is not None
-    assert result["fee"] is not None
-    assert result["confirmation"] is not None
-    assert "➕" in result["confirmation"]
-    assert "⛽" in result["confirmation"]
-    assert "🗓️" in result["confirmation"]
-    assert "ℹ️" in result["confirmation"]
-
-
-@pytest.mark.asyncio
-async def test_get_approve(dex_client):
-    symbol = "UNI"
-    approve_receipt = None
-    try:
-        approve_receipt = await dex_client.account.get_approve(symbol)
-        print(approve_receipt)
-    except Exception as e:
-        print(f"Error getting approve receipt: {e}")
-    assert approve_receipt is None
-
-
-@pytest.mark.asyncio
-async def test_get_cg_data(dex_client):
-    get_cg_data = AsyncMock()
-    result = await dex_client.get_quote(symbol="LINK")
-    assert result is not None
-    assert isinstance(result, float)
-    assert get_cg_data.awaited
-
-
-@pytest.mark.asyncio
-async def test_get_token_exception(dex_client, caplog):
-    await dex_client.get_quote(symbol="NOTATHING")
-    assert "Quote failed" in caplog.text
