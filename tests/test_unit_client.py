3ea9d2c632d3d71ee67789c92fa04b8d878c9e3c
mraniki <8766259+mraniki@users.noreply.github.com>
2023-10-18 19:58:31 +0200

    ✅ Unit Test

--- tests/test_unit_client.py
+++ tests/test_unit_client.py
@@ -7,13 +7,12 @@ from unittest.mock import AsyncMock, MagicMock
 import pytest
 
 from dxsp import DexSwap
-from dxsp.protocols.uniswap import DexUniswap
 from dxsp.config import settings
 
 
 @pytest.fixture(scope="session", autouse=True)
 def set_test_settings():
-    settings.configure(FORCE_ENV_FOR_DYNACONF="dxsp")
+    settings.configure(FORCE_ENV_FOR_DYNACONF="uniswap")
 
 
 @pytest.fixture(name="dex")
@@ -27,28 +26,6 @@ def client_fixture(dex):
         if dx.protocol == "uniswap":
             return dx
 
-# @pytest.fixture(name="dex_client")
-# def client_fixture(dex):
-#     return DexUniswap(
-#         name="uniswap",
-#                     wallet_address=_config.get("wallet_address"),
-#                     private_key=_config.get("private_key"),
-#                     rpc=_config.get("rpc"),
-#                     w3=Web3(Web3.HTTPProvider(_config.get("rpc"))),
-#                     protocol=protocol,
-#                     protocol_version=_config.get("protocol_version") or 2,
-#                     api_endpoint=_config.get("api_endpoint") or "https://api.0x.org/",
-#                     api_key=_config.get("api_key") or None,
-#                     router_contract_addr= None,
-#                     factory_contract_addr= None,
-#                     trading_risk_percentage= True,
-#                     trading_risk_amount=  1,
-#                     trading_slippage= 2,
-#                     trading_asset_address=,
-#                     trading_asset_separator= "",
-#                     block_explorer_url=_config.get("block_explorer_url") or None,
-#                     block_explorer_api=_config.get("block_explorer_api") or None,
-#                     mapping=_config.get("mapping") or None,)
 
 def test_dynaconf_is_in_testing():
     print(settings.VALUE)
